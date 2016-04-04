b6b8a110766a453243bb9aa35496774b0a70cd0a
Tkalakota <tejak3525@gmail.com>
2016-04-04 10:41:11 -0400

    Added files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    /* For parallaxing images */
+    $('.parallax').parallax();
+
+    /* To scroll to a section from navbar */
+    $('.scrollspy').scrollSpy();
+});
