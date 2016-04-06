4e6b010be87024bdb3c69d1e2b4a9a5fea49f748
Tkalakota <tejak3525@gmail.com>
2016-04-06 10:27:55 -0400

    Added files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Scripts for rows.html
+
+$('.animate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated fadeIn");
+}).on('mouseleave', function() {
+  $(this).toggleClass("animated fadeIn");
+});
