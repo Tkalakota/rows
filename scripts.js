5c816ffa9b46a9788548ac992d68e38b2f7281c0
Tkalakota <tejak3525@gmail.com>
2016-04-06 10:26:59 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    /* For parallaxing images */
-    $('.parallax').parallax();
-
-    /* To scroll to a section from navbar */
-    $('.scrollspy').scrollSpy();
-});
